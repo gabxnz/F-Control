3b3f1b363c3d1367c30be67120fbd62db8b1dfc0
gabxnz <gabrielcosta@sempreceub.com>
2026-04-10 17:40:58 -0300

    github actions

--- financeiro.test.js
+++ financeiro.test.js
@@ -4,3 +4,17 @@ test('deve calcular o saldo final corretamente', () => {
   const saldo = receitas - despesas;
   expect(saldo).toBe(1200);
 });
+
+// Teste 2: Entrada Inválida (Valor negativo)
+test('deve impedir ou identificar valor negativo', () => {
+    const valorDigitado = -50;
+    const ehValido = valorDigitado > 0;
+    expect(ehValido).toBe(false);
+});
+
+// Teste 3: Caso Limite (Saldo Zero)
+test('deve permitir que o saldo fique exatamente zero', () => {
+    const despesa = 500;
+    const saldo = 500;
+    expect(saldo - despesa).toBe(0);
+});
